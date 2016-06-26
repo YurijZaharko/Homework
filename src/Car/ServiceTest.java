dd83a365d4381b6e980f134e618fab6f4fa4eb16
ZakharkoYurij <scipgames@gmail.com>
2016-06-26 17:40:20 +0300

    ДЗ: Ваша ціль протестувати 2 попередніх завдання в середньому на 65%, проте зараховувати буду і 50% звісно чим більше покриття коду тестами тим краще для Вас. Успіхів. Для того щоб досягти такого результату Вам потрібно відділити логіку зчитування даних введених користувачем від логіки обробки і виконання. Тобто, одна частина програми зчитує дані, а інша іх обробляє і виконує.

---
+++ src/Car/ServiceTest.java
@@ -0,0 +1,74 @@
+package Car;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by SCIP on 26.06.2016.
+ */
+public class ServiceTest {
+    @org.junit.Test
+    public void updateListOfCar() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void findCar() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void findCar1() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void changeInRedCars() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void changeSizeOfWheels() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void changeCarWithSizeOfWheels() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void readInt() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void readString() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void findCar2() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void removeCarIfColor() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void changeWheelsToWinter() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void removeCarIfBodyAndWheels() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getIndexIfBody() throws Exception {
+
+    }
+
+}
